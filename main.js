863e1ec3d20787cad9b3a0ff9fb1648047e142cd
Oxygen_bottle（home） <810104157@qq.com>
2022-04-29 13:53:54 +0800

    修改不能启动问题

--- main.js
+++ main.js
@@ -12,25 +12,23 @@ function createWindow() {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-
-  // 加载 index.html
   mainWindow.loadFile('./dist/index.html') // 此处跟electron官网路径不同，需要注意
-
-  // 打开开发工具
-  // mainWindow.webContents.openDevTools()
-  app.whenReady().then(() => {
-    createWindow()
-
-    app.on('activate', function () {
-      // 通常在 macOS 上，当点击 dock 中的应用程序图标时，如果没有其他
-      // 打开的窗口，那么程序会重新创建一个窗口。
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-  })
-
-  // 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 因此，通常对程序和它们在
-  // 任务栏上的图标来说，应当保持活跃状态，直到用户使用 Cmd + Q 退出。
-  app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
-  })
 }
+
+// 打开开发工具
+// mainWindow.webContents.openDevTools()
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    // 通常在 macOS 上，当点击 dock 中的应用程序图标时，如果没有其他
+    // 打开的窗口，那么程序会重新创建一个窗口。
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 因此，通常对程序和它们在
+// 任务栏上的图标来说，应当保持活跃状态，直到用户使用 Cmd + Q 退出。
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
